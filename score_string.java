affca0bfe12c8baa789c5ec07c48a8408e981f85
Gopika <gopikasivakumar06@gmail.com>
2026-03-09 20:58:54 +0530

    Add files via upload

---
+++ score_string.java
@@ -0,0 +1,21 @@
+public class score_string {
+
+    public static int scoreOfString(String s) {
+        int score = 0;
+
+        for(int i = 0; i < s.length() - 1; i++){
+            score += Math.abs(s.charAt(i) - s.charAt(i + 1));
+        }
+
+        return score;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "code";
+
+        int result = scoreOfString(s);
+
+        System.out.println("Score of string = " + result);
+    }
+}
